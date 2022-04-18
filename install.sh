47004eecda8b31c8b026b88ed5d3fbc73d9153af
cat-master21 <96554164+cat-master21@users.noreply.github.com>
2022-04-17 18:40:05 -0700

    fix(install.sh): fix sudo use

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ pacstall -PI yad
 sudo mkdir -p /usr/lib/pacstall-gui  && cd /usr/lib/pacstall-gui
 sudo wget -q https://github.com/cat-master21/pacstall-gui/raw/master/disable-root-check.diff | sudo wget -q https://github.com/cat-master21/pacstall-gui/raw/master/settings.sh
 
-wget -q --show-progress --progress=bar:force -O "/usr/bin/pacstall-gui" "https://github.com/cat-master21/pacstall-gui/raw/master/pacstall-gui"
-chmod +x "/usr/bin/pacstall-gui"
+sudo wget -q --show-progress --progress=bar:force -O "/usr/bin/pacstall-gui" "https://github.com/cat-master21/pacstall-gui/raw/master/pacstall-gui"
+sudo chmod +x "/usr/bin/pacstall-gui"
